782fe51584c56c879fced5e8dac177e3a9effd32
Alireza Gharib <alirezagharib110@gmail.com>
2025-07-17 19:14:41 +0330

    aggregator is fully implemented

---
+++ aggregator/client/client.go
@@ -0,0 +1,12 @@
+package client
+
+import (
+	"context"
+
+	"TripCost/types"
+)
+
+type Client interface {
+	Aggregate(context.Context, *types.AggregateRequest) error
+	GetInvoice(context.Context, int) (*types.Invoice, error)
+}
